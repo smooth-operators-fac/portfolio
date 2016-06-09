e4d08f5a0c186fcdad67a7aabc776bc9e64c8f0a
bradreeder <bradjohnreeder@gmail.com>
2016-06-09 22:21:23 +0100

    modified profile page

--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,67 @@
 (function() {
 
-var modal = document.getElementById('myModal'),
-    close = document.getElementsByClassName('close')[0];
-    content = document.getElementsByClassName('modal-content')[0];
+    var modal = document.getElementById('myModal'),
+        close = document.getElementsByClassName('close')[0];
+        content = document.getElementsByClassName('modal-content')[0];
 
-var bradley = document.getElementById('bradley'),
-    sam = document.getElementById('sam'),
-    mattia = document.getElementById('mattia'),
-    jakub = document.getElementById('jakub');
+    var bradley = document.getElementById('bradley'),
+        sam = document.getElementById('sam'),
+        mattia = document.getElementById('mattia'),
+        jakub = document.getElementById('jakub');
 
-bradley.onclick = function() {
+    function createModal (imageSrc, biography) {
+        //create the biographical modal text
+        var bio = document.createElement('p');
+        var description = document.createTextNode(biography);
+        bio.className = 'bioText';
+        bio.appendChild(description);
+        //create the biographical modal image
+        var image = document.createElement('img');
+        image.src = imageSrc;
+        image.className = "bioImages";
+        //append both to the modal-content div
+        content.appendChild(image);
+        content.appendChild(bio);
+        //adds a links section
+        //content.innerHTML += '<p>You can find me on github, linkedin and twitter</p>'
+        //change the modals opacity and zIndex so it appears on screen
+        modal.style.opacity = '1';
+        modal.style.zIndex = '1';
+    }
+
+    bradley.addEventListener('click', function() {
+        createModal("resources/bradley.jpg", 'Bradley graduated with an MA in literature in 2013. When not coding he loves writing and travelling. Hes especially fascinated with the comics medium and hopes to one day use his web-development skills to build novel hyper-comics on the web and using mobile location-based technologies.');
+    });
+    sam.addEventListener('click', function() {
+        createModal("resources/sam.png", 'Sam is learning JavaScript with Founders and Coders and attempting to teach himself Python and C. He is becoming increasingly interested in data science and machine learning, though his current pet project is a website for making and sharing semiotic squares. Sam also moonlights as an expert on Renaissance alchemy and natural magic.');
+    });
+    mattia.addEventListener('click', function() {
+        createModal("resources/mattia.jpg", 'After sinking 2 startups its due time for me to actually learn to code. I am doing it here in London at Founders and Coders, hope the name will bring me luck. When not coding for the web I like to learn about functional programming, machine learning and lately the blockchain. Non geeky interests include watching documentaries, riding a bike and planning the next trip.');
+    });
+    jakub.addEventListener('click', function() {
+        createModal("resources/jakub.jpg", 'This is Jakub\'s bio and he is not a red panda. He does however aspire to become Batman one day and take justice into his own hands. He is learning HTML, CSS and JavaScript with the Founders and Coders lot. Hopefully, he will not die before his time comes.');
+    });
+
+    close.onclick = function() {
+        content.removeChild(content.lastChild);
+        content.removeChild(content.lastChild);
+        modal.style.opacity = '0';
+        modal.style.zIndex = '-99';
+    }
+    window.onclick = function() {
+        if (event.target == modal) {
+            content.removeChild(content.lastChild);
+            content.removeChild(content.lastChild);
+            modal.style.zIndex = '-99';
+            modal.style.opacity = '0';
+        }
+    }
+
+})();
+
+/*  -- original event functions in case we need a back-up --
+
+  bradley.onclick = function() {
     var bio = document.createElement('p');
         bio.className = "bioText";
     var description = document.createTextNode('Bradley graduated with an MA in literature in 2013. When not coding he loves writing and travelling. Hes especially fascinated with the comics medium and hopes to one day use his web-development skills to build novel hyper-comics on the web and using mobile location-based technologies.');
@@ -71,21 +123,4 @@ jakub.onclick = function() {
     content.appendChild(bio);
     modal.style.opacity = '1';
     modal.style.zIndex = '1';
-}
-
-close.onclick = function() {
-    content.removeChild(content.lastChild);
-    content.removeChild(content.lastChild);
-    modal.style.opacity = '0';
-    modal.style.zIndex = '-99';
-}
-window.onclick = function() {
-    if (event.target == modal) {
-        content.removeChild(content.lastChild);
-        content.removeChild(content.lastChild);
-        modal.style.zIndex = '-99';
-        modal.style.opacity = '0';
-    }
-}
-
-})();
+} */
